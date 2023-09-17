5c1ee014d1f9fdc7e19bd7106796dd7501d36268
ThangPham <phamducthang2503200@gmail.com>
2023-09-17 11:42:47 +0700

    Add project files.

---
+++ ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StoreManagerApp.ViewModel
+{
+    public class ControlBarViewModel
+    {
+        #region
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommnand { get; set; }
+        public ICommand MouseMoveWindowCommand { get; set; }
+        #endregion
+        public ControlBarViewModel() 
+        {
+            CloseWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => { });
+        }
+        private FrameworkElement GetParent(FrameworkElement parent)
+        {
+            FrameworkElement Parent = parent;
+            while (Parent.Parent != null) 
+            {
+                Parent = Parent.Parent as FrameworkElement;
+            }
+            return Parent;
+        }
+
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using StoreManagerApp.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StoreManagerApp.ViewModel
+{
+    public class MainViewModel
+    {
+        public bool IsLoaded { get; set; } = false;
+        public ICommand LoadedWindowCommad { get; set; }
+
+        public MainViewModel() 
+        {
+            LoadedWindowCommad = new RelayCommand<object>((p) => { return true; }, (p) => { Loaded(); });
+        }
+        public void Loaded()
+        {
+            IsLoaded = true;
+            LoginScreen loginScreen = new LoginScreen();
+            loginScreen.ShowDialog();
+        }
+    }
+}
