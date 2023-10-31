dd7fd8814e92a468ecc86c7414c3e312f3a34cf8
ThangPham <phamducthang2503200@gmail.com>
2023-10-31 22:27:28 +0700

    add comment

--- ViewModel/SuplierViewModel.cs
+++ ViewModel/SuplierViewModel.cs
@@ -14,6 +14,9 @@ namespace StoreManagerApp.ViewModel
 {
     public class SuplierViewModel : BaseViewModel
     {
+        /**
+         * Define Private property
+         */
         #region Private Property
         private string _DisplayName { get; set; }
         private string _Address { get; set; }
@@ -25,6 +28,10 @@ namespace StoreManagerApp.ViewModel
         private ObservableCollection<Suplier> _List;
         private Suplier _SelectedItem;
         #endregion
+
+        /**
+         * Define Public Property
+         */
         #region Public Property
         public string DisplayName 
         {
@@ -135,17 +142,39 @@ namespace StoreManagerApp.ViewModel
             }
         }
         #endregion
+
+        /**
+         * Define Command
+         */
         #region Command
         public ICommand AddCommand { get; set; }
         public ICommand EditCommand { get; set; }
         public ICommand DeleteCommand { get; set; }
         #endregion
+
+        #region Contructor and Detructor
+        /// <summary>
+        /// Contructor of class <c>SuplierViewModel</c>
+        /// </summary>
         public SuplierViewModel()
         {
             _List = new ObservableCollection<Suplier>(DataProvider.Ins.DB.Suplier);
             AddCommand = new RelayCommand<object>(CheckAddData, AddSuplier);
             EditCommand = new RelayCommand<object>(CheckEditData, EditSuplier);
         }
+
+        /// <summary>
+        /// Detructor of class <c>SuplierViewModel</c>
+        /// </summary>
+        ~SuplierViewModel()
+        {
+
+        }
+        #endregion
+
+        /// <summary>
+        /// Method to show data when open suplier window
+        /// </summary>
         private void ShowData()
         {
             if (SelectedItem != null)
@@ -158,11 +187,23 @@ namespace StoreManagerApp.ViewModel
                 ContractDate = (DateTime)SelectedItem.ContractDate;
             }
         }
+
+        /// <summary>
+        /// Method to enable add button
+        /// </summary>
+        /// <param name="sender">Param get value from command parameter</param>
+        /// <returns>Return <c>true</c> if no data in database</returns>
         private bool CheckAddData(object sender)
         {
             bool RetValue = CheckNullData() && IsHaveData(UIButtonType.Add); 
             return RetValue;
         }
+
+        /// <summary>
+        /// The method used to check whether data already exists in the database or not? 
+        /// </summary>
+        /// <param name="type">button type</param>
+        /// <returns>return <c>true</c> if not find data in database</returns>
         private bool IsHaveData(UIButtonType type)
         {
             bool Ret = false;
@@ -202,6 +243,11 @@ namespace StoreManagerApp.ViewModel
             }
             return Ret;
         }
+
+        /// <summary>
+        /// The method used to add data to the database
+        /// </summary>
+        /// <param name="sender">Param get value from command parameter</param>
         private void AddSuplier(object sender)
         {
             Suplier NewSuplier = new Suplier
@@ -217,6 +263,11 @@ namespace StoreManagerApp.ViewModel
             DataProvider.Ins.DB.SaveChanges();
             ListSuplier.Add(NewSuplier);
         }
+
+        /// <summary>
+        /// Method used to check data input is null or not
+        /// </summary>
+        /// <returns>Return <c>true</c> if data not null</returns>
         private bool CheckNullData()
         {
             if (string.IsNullOrEmpty(Email)         ||
@@ -234,6 +285,12 @@ namespace StoreManagerApp.ViewModel
             }
             return true;
         }
+
+        /// <summary>
+        /// Method used to check data to edit
+        /// </summary>
+        /// <param name="sender">Param get value from command parameter</param>
+        /// <returns>Return <c>true</c> if not find data in database</returns>
         private bool CheckEditData(object sender)
         {
             bool RetValue = false;
@@ -243,6 +300,11 @@ namespace StoreManagerApp.ViewModel
             }
             return RetValue;
         }
+
+        /// <summary>
+        /// Method used to edit data
+        /// </summary>
+        /// <param name="sender">Param get value from command parameter</param>
         private void EditSuplier(object sender) 
         {
             var data = DataProvider.Ins.DB.Suplier.Where(item => item.Id == Id).SingleOrDefault();
